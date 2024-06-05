f80a13f5bcde58219c45e5453a423bd2eff7513a
VortAmplifier <163753936+VortAmplifier@users.noreply.github.com>
2024-06-05 18:41:35 -0400

    Add files via upload

---
+++ remove_char_string.py
@@ -0,0 +1,7 @@
+# A program that removes certain characters from a given string includes a function
+def remove_chars(string, n):
+	new_string = string[n:]
+	
+	return new_string
+	
+print(remove_chars("pynative", 2))
