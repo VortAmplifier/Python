ce8c4c2b16404993db460f391e192fc22ac514d3
VortAmplifier <163753936+VortAmplifier@users.noreply.github.com>
2024-06-06 08:26:39 -0400

    Update string_char.py

--- string_char.py
+++ string_char.py
@@ -1,6 +1,7 @@
 # Print characters from a string that are present at an even index number
-str = input("Original String is  ")
+# https://pynative.com/python-basic-exercise-for-beginners/ / Exercise 3
+str = input("Original String is  ") # input function, enter a string
 
-for index in range(len(str)):
-	if index % 2 == 0:
-		print(str[index])
+for index in range(len(str)): # for loop 
+	if index % 2 == 0: # test even number
+		print(str[index]) # print result
