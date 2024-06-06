0470d44721ff013cf3a2506ad8b1b884eccb1ef6
VortAmplifier <163753936+VortAmplifier@users.noreply.github.com>
2024-06-06 08:24:32 -0400

    Update remove_char_string.py

--- remove_char_string.py
+++ remove_char_string.py
@@ -1,5 +1,5 @@
 # A program that removes certain characters from a given string includes a function
-# https://pynative.com/python-basic-exercise-for-beginners/ / Exercise 3
+# https://pynative.com/python-basic-exercise-for-beginners/ / Exercise 4
 def remove_chars(string, n): # define a function remove_chars that accepts two arguments string and a number
 	new_string = string[n:] # Use string slicing
 	
