194985ed8a629c90928c3c62c4403dffe5f072e6
VortAmplifier <163753936+VortAmplifier@users.noreply.github.com>
2024-06-08 14:43:17 -0400

    Add files via upload
    Merge strings from two lists into a third list

---
+++ merge_two_lists_strings.py
@@ -0,0 +1,44 @@
+# Enter values and place them in a list
+# Merge strings from two lists into a third list
+
+string1 = input()
+string2 = input()
+
+lst1 = []
+lst2 = []
+
+for index in range(len(string1)):
+	if string1[index] == " ":
+		continue
+	else:
+		lst1.append(string1[index])
+		
+for index in range(len(string2)):
+	if string2[index] == " ":
+		continue
+	else:
+		lst2.append(string2[index])
+		
+print(f"List one: {lst1}")
+print(f"List two: {lst2}")
+
+lst3 = []
+
+if len(lst1) > len(lst2):
+	for index in range(len(lst1)):
+		while index <= (len(lst2) - 1):
+			lst3.append(lst1[index] + lst2[index])
+	lst3.append(lst1[len(string1) - 1])
+
+if len(lst2) > len(lst1):
+	for index in range(len(lst2)):
+		while index <= (len(lst2) - 1):
+			lst3.append(lst1[index] + lst2[index])
+	lst3.append(lst2[len(lst2) - 1])
+
+if len(lst1) == len(lst2):
+	for index in range(len(lst1)):
+		lst3.append(lst1[index] + lst2[index])
+
+print(lst3)
+
