e2d5bd493f0038e5525bae541cfb175b7c431575
VortAmplifier <163753936+VortAmplifier@users.noreply.github.com>
2025-03-21 09:38:10 -0400

    Add files via upload

---
+++ FavoriteColor.py
@@ -0,0 +1,14 @@
+https://www.youtube.com/watch?v=_uQrJ0TkZlc&t=109s / Mosh Hamedani's youtube course
+
+Sample solution no ChatGPT
+---------------------------------------------------------
+# A program that prints person's name and favorite color
+name = input("Enter a name: ")
+color = input("Enter favorite color: ")
+print(name + "\'s" + " favorite color" + " is " + color + ".")
+
+Terminal
+--------
+Enter a name: John Doe
+Enter favorite color: Hazel
+John Doe's favorite color is Hazel.
